51d3ff5efdce58ebd33bda8231c0e02224aa14e9
Mateusz Machaj <mattx264@yahoo.com>
2016-07-07 08:56:00 -0500

    add show password

---
+++ src/showPassword/showPassword.js
@@ -0,0 +1,25 @@
+﻿(function () {
+    'use strict';
+    function showPassword() {
+        return {
+            scope: {},
+            link: function (scope, element, attrs, ctrl) {
+                //init show password
+                var passwordVisible = false;
+                element.bind('click', function() {
+                    if (passwordVisible == false) {
+                        $('#' + attrs.cecpasswordid).removeAttr('type');
+                        $('#' + attrs.cecpasswordid).prop('type', 'text');
+                        passwordVisible = true;
+                    } else {
+                        $('#' + attrs.cecpasswordid).removeAttr('type');
+                        $('#' + attrs.cecpasswordid).prop('type', 'password');
+                        passwordVisible = false;
+                    }
+                });
+
+            }
+        }
+    }
+    angular.module('ui.controls').directive('showPassword', [cecShowPassword]);
+})();
