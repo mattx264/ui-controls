75405ae4a2373c015d0dd772a6ae1da2dae4a75b
Mateusz Machaj <mattx264@yahoo.com>
2016-07-20 09:12:57 -0500

    update message

--- gulpfile.js
+++ gulpfile.js
@@ -5,10 +5,10 @@ var gulp = require('gulp'),
   templateCache = require('gulp-angular-templatecache'),
   sass = require('gulp-sass');
 
-var src='./src/**/*.js',
-    dest='./dist/'
+var src = './src/**/*.js',
+  dest = './dist/'
 
-    
+
 gulp.task('min', function () {
   gulp.src(src)
     .pipe(concat('ui-controls.min.js'))
@@ -18,23 +18,24 @@ gulp.task('min', function () {
 })
 gulp.task('default', function () {
   gulp.src('./src/**/*.html')
-    .pipe(templateCache('templates.js',{'root':'src','module':"ui.controls"}))
+    .pipe(templateCache('templates.js', { 'root': 'src', 'module': "ui.controls" }))
     .pipe(gulp.dest(dest))
-   gulp.src(src)
+  gulp.src(src)
     .pipe(concat('ui-controls.js'))
     .pipe(gulp.dest(dest));
-     gulp.src(dest+'*.js')
-     .pipe(gulp.dest('./demo/js'));
-     
+  gulp.src(dest + '*.js')
+    .pipe(gulp.dest('./demo/js'));
+
 });
 gulp.task('sass', function () {
-   gulp.src(dest+'*.css').pipe(gulp.dest('./demo/css'))
-  return gulp.src('./src/**/*.scss')
+
+  gulp.src('./src/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-      .pipe(concat('style.css'))
+    .pipe(concat('style.css'))
     .pipe(gulp.dest('./dist/'));
+  return gulp.src(dest + '*.css').pipe(gulp.dest('./demo/css'));
 });
- 
+
 gulp.task('sass:watch', function () {
   gulp.watch('./src/**/*.scss', ['sass']);
 });
--- src/message/error-message.js
+++ src/message/error-message.js
@@ -4,7 +4,7 @@
           return {
             restrict: 'E',
             transclude:true,
-            template: '<div class="well well-sm message-error">  <span class="icon icon-x-circle"></span><div class="well-text" ng-transclude></div></div>',
+            template: '<div class="well well-sm ui-message-error">  <span class=""></span><div class="well-text" ng-transclude></div></div>',
         };
     }
     angular.module('ui.controls').directive('errorMessage', [errorMessage]);
